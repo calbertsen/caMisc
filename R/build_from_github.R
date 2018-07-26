f924c809ad7f6286d3b7833b9f0cb854c16ec313
Christoffer Moesgaard Albertsen <ca@calbertsen.dk>
2018-07-26 16:02:03 +0200

    Bugfix in buildfromgithub?

--- R/build_from_github.R
+++ R/build_from_github.R
@@ -59,7 +59,7 @@ installDependencies <- function(descriptionPath,
     }
     if(length(dependencies) > 0){
         allDep <- unlist(strsplit(paste(na.omit(dcf[,dependencies]),collapse=", "),",[[:space:]]*"))
-        allDepTab <- unique(t(sapply(allDep,function(d){r <- strsplit(gsub("(\\(|\\))","",d),"[[:space:]]")[[1]];c(r,NA,NA)[1:3]})))
+        allDepTab <- unique(matrix(t(sapply(allDep,function(d){r <- strsplit(gsub("(\\(|\\))","",d),"[[:space:]]")[[1]];c(r,NA,NA)[1:3]})),ncol = 3))
         allDepTab <- allDepTab[!(allDepTab[,1] %in% c("R","",NA)),,drop=FALSE]
         allDepList <- split(as.data.frame(allDepTab),allDepTab[,1],drop=FALSE)
         ## Handle same package multiple times
