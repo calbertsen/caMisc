0925dec874d43d939335b0730e7ea4acd6edeed3
Christoffer Moesgaard Albertsen <ca@calbertsen.dk>
2018-12-06 12:28:22 +0100

    Update build_from_github.R
    Try new github url

--- caMisc/R/build_from_github.R
+++ caMisc/R/build_from_github.R
@@ -27,7 +27,8 @@ buildFromGithub <- function(repo,
     user <- splitRepo[1]
     urep <- splitRepo[2]
     fil <- file.path(topdir,paste0(urep,"_",ref,".zip"))
-    url <- sprintf("https://github.com/%s/archive/%s.zip",repo,ref)
+    ##url <- sprintf("https://github.com/%s/archive/%s.zip",repo,ref)
+    url <- sprintf("https://codeload.github.com/%s/zip/%s",repo,ref)
     utils::download.file(url,fil,quiet=TRUE)
     a <- utils::unzip(fil,exdir = topdir)
     descriptionPath <- a[grepl(paste0(subdir,"/DESCRIPTION"),a)]
